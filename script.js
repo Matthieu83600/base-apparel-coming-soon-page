1310151cd9d7b1eec10a5cb3fce4238576cdb69b
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-06-17 14:09:50 +0200

    refactor: clean up js

--- script.js
+++ script.js
@@ -7,32 +7,46 @@ const formWrapper = document.querySelector(".form__wrapper");
 const errorIcon = document.querySelector(".error-icon");
 const message = document.querySelector(".message");
 
-let errorTimeout;
+let feedbackTimeout;
+
+const successMessage = () => {
+  message.innerHTML = `<span>Your email has been sent</span>`;
+  message.style.display = "block";
+  message.style.color = "green";
+};
+
+const errorMessage = () => {
+  message.innerHTML = `<span>Please provide a valid email</span>`;
+  message.style.display = "block";
+  message.style.color = "#f96262";
+};
+
+const clearMessage = () => {
+  message.style.display = "none";
+  message.innerHTML = "";
+};
 
 const submitForm = (e) => {
   e.preventDefault();
+  clearTimeout(feedbackTimeout);
   if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(emailInput.value)) {
     emailInput.value = "";
-    message.innerHTML = `<span>Your email has been sent</span>`;
-    message.style.display = "block";
-    message.style.color = "green";
     formWrapper.classList.add("form__wrapper--success");
+    successMessage();
     setTimeout(() => {
       formWrapper.classList.remove("form__wrapper--success");
-      message.style.display = "none";
+      clearMessage();
     }, 2000);
   } else {
     formWrapper.classList.add("form__wrapper--error");
     errorIcon.classList.add("error-icon-show");
-    message.innerHTML = `<span>Please provide a valid email</span>`;
-    message.style.display = "block";
-    message.style.color = "#f96262";
     emailInput.setAttribute("aria-invalid", "true");
+    errorMessage();
     setTimeout(() => {
       formWrapper.classList.remove("form__wrapper--error");
       errorIcon.classList.remove("error-icon-show");
-      message.style.display = "none";
       emailInput.removeAttribute("aria-invalid");
+      clearMessage();
     }, 2000);
   }
 };
